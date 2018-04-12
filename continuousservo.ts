d37a083d6710c92570f20d281d92ca0f803a7516
YJ Soon <yjsoon@tinkertanker.com>
2018-04-12 11:44:36 +0800

    Updated weights that were reversed

--- continuousservo.ts
+++ continuousservo.ts
@@ -10,7 +10,7 @@ namespace ContinuousServo {
     * Spins the motor in one direction at full speed
     * @param pin Which pin the motor is on
     */
-    //% blockId=spin_one_way weight=10
+    //% blockId=spin_one_way weight=100
     //% block="spin one way pin %pin"
     export function spin_one_way(pin: AnalogPin): void {
       pins.servoWritePin(pin, 180)
@@ -20,7 +20,7 @@ namespace ContinuousServo {
     * Spins the motor in other direction at full speed
     * @param pin Which pin the motor is on
     */
-    //% blockId=spin_other_way weight=20
+    //% blockId=spin_other_way weight=80
     //% block="spin other way pin %pin"
     export function spin_other_way(pin: AnalogPin): void {
       pins.servoWritePin(pin, 0)
@@ -31,7 +31,7 @@ namespace ContinuousServo {
     * @param pin Which pin the motor is on
     * @param speed Speed from 0 to 100
     */
-    //% blockId=spin_one_way_with_speed weight=30
+    //% blockId=spin_one_way_with_speed weight=60
     //% block="spin one way pin %pin | with speed %speed"
     //% speed.min=0 speed.max=100
     export function spin_one_way_with_speed(pin: AnalogPin, speed: number): void {
@@ -56,7 +56,7 @@ namespace ContinuousServo {
     * Turns off the motor at this pin
     * @param pin Which pin the motor is on
     */
-    //% blockId=turn_off_motor weight=50
+    //% blockId=turn_off_motor weight=20
     //% block="turn off motor at pin %pin"
     export function turn_off_motor(pin: AnalogPin): void {
       pins.digitalWritePin(pin, 0)
